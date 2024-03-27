a5e24052cbf3b01e3d741a6f517ba569c6c6bea6
Sebastian Jensen <apps@tsjdev-apps.de>
2024-03-27 08:13:20 +0100

    Add empty LottieAnimation component.

---
+++ src/angular-lottie-animations/src/app/lottie-animation/lottie-animation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lottie-animation',
+  standalone: true,
+  imports: [],
+  templateUrl: './lottie-animation.component.html',
+  styleUrl: './lottie-animation.component.css'
+})
+export class LottieAnimationComponent {
+
+}
