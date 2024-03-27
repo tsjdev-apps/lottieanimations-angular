87f45c22cca10533d29ff8164773393f6fb5077d
Sebastian Jensen <apps@tsjdev-apps.de>
2024-03-27 08:50:37 +0100

    Update code on LottieAnimation component to work properly.

--- src/angular-lottie-animations/src/app/lottie-animation/lottie-animation.component.ts
+++ src/angular-lottie-animations/src/app/lottie-animation/lottie-animation.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
 
 @Component({
   selector: 'app-lottie-animation',
   standalone: true,
-  imports: [],
+  imports: [LottieComponent],
   templateUrl: './lottie-animation.component.html',
   styleUrl: './lottie-animation.component.css'
 })
 export class LottieAnimationComponent {
+  private animationItem: AnimationItem | undefined;
 
+  options: AnimationOptions = {
+    path: '/assets/animations/globe.json',
+    loop: true,
+    autoplay: false
+  };
+
+  animationCreated(animationItem: AnimationItem): void {
+    this.animationItem = animationItem;
+  }
+
+  play(): void {
+    if (this.animationItem) {
+      this.animationItem.play();
+    }
+  }
+
+  pause(): void {
+    if (this.animationItem) {
+      this.animationItem.pause();
+    }
+  }
+
+  stop(): void {
+    if (this.animationItem) {
+      this.animationItem.stop();
+    }
+  }
 }
