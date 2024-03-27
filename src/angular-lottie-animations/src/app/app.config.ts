695bef8f7c3a853e4f4f5846cbf933c0ad4ae54b
Sebastian Jensen <apps@tsjdev-apps.de>
2024-03-27 08:11:15 +0100

    Register Lottie in app.config.ts file.

--- src/angular-lottie-animations/src/app/app.config.ts
+++ src/angular-lottie-animations/src/app/app.config.ts
@@ -3,6 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { provideCacheableAnimationLoader, provideLottieOptions } from 'ngx-lottie';
+import player from 'lottie-web';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideLottieOptions({ player: () => player }),
+    provideCacheableAnimationLoader(),
+    provideRouter(routes)
+  ]
 };
